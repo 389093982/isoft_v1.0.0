4e86c73014b0811740cc0e9f8936669e544784b3
389093982 <389093982@qq.com>
2018-06-02 23:49:28 +0800

    初次提交

--- isoft/build/syncfile/sync/sync_file_test.go
+++ isoft/build/syncfile/sync/sync_file_test.go
@@ -9,14 +9,14 @@ import (
 
 func Test_SyncFile_Static(t *testing.T) {
 	gopath := os.Getenv("GOPATH")
-	SyncFile := ReadSyncFile( filepath.Join(gopath, "src/isoft/isoft/syncfile/sync/static.xml"))
+	SyncFile := ReadSyncFile( filepath.Join(gopath, "src/isoft/isoft/build/syncfile/sync/static.xml"))
 	StartAllSyncFile(gopath, SyncFile, "")
 	time.Sleep(60*time.Second)
 }
 
 func Test_SyncOneFile_Static(t *testing.T) {
 	gopath := os.Getenv("GOPATH")
-	SyncFile := ReadSyncFile( filepath.Join(gopath, "src/isoft/isoft/syncfile/sync/static.xml"))
+	SyncFile := ReadSyncFile( filepath.Join(gopath, "src/isoft/isoft/build/syncfile/sync/static.xml"))
 	StartAllSyncFile(gopath, SyncFile, "isoft_deploy_web")
 
 	time.Sleep(60*time.Second)
